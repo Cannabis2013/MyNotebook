85491cd4508eb1937110af39ecb3b129899d03c6
MH2012 <MH2012@outlook.dk>
2024-11-22 10:41:37 +0100

    Separated api key

---
+++ firebaseConfig.js
@@ -0,0 +1,25 @@
+import "firebase/compat/storage"
+import { initializeApp } from "firebase/app";
+import { getStorage } from "@firebase/storage";
+import {firebaseApiKey} from "./credentials/authCredentials"
+let app = undefined
+
+export function getFirebaseApp () {
+    if (app)
+        return app
+    const firebaseConfig = {
+        apiKey: firebaseApiKey,
+        authDomain: "mynotebook-6e40c.firebaseapp.com",
+        projectId: "mynotebook-6e40c",
+        storageBucket: "gs://mynotebook-6e40c.appspot.com",
+        messagingSenderId: "874195159834",
+        appId: "1:874195159834:web:3d9affc4b36f2aa434faa3"
+    };
+    app = initializeApp(firebaseConfig);
+    return app
+}
+
+export function getStorageReference(){
+    const app = getFirebaseApp()
+    return getStorage(app)
+}
